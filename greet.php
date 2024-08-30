5ffc773ae30717a572f8455523394ef38ff43ac2
Md Shahid Ansari <128293212+md-shahid-ansari@users.noreply.github.com>
2024-08-30 11:51:45 +0530

    Add files via upload

--- greet.php
+++ greet.php
@@ -1,7 +1,21 @@
-<?PHP>{
-    $name = $_POST["name"]
-    $n = $_POST["number"]
-    for ($i=1; $i < $n ; $i++) { 
-        echo "Hello ".$name."<br>"
-    }
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Greet!</title>
+    <link rel="stylesheet" type="text/css" href="greet.css">
+</head>
+<body>
+    <?php
+        $name = $_POST["name"];
+        $n = $_POST["number"];
+        $msg = $_POST["msg"];
+        echo '<div class="output">';
+        for ($i = 1; $i <= $n; $i++) {
+            echo "<span>".$msg. ", " .$name. "</span><br>";
+        }
+        echo '</div>';
+    ?>
+</body>
+</html>
